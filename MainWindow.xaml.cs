cba46282c1f5bfcf740f047b3095ef034370647e
Svetlana Savich <Svetlana_Savich@epam.com>
2022-11-07 11:41:56 +0100

    Fixed version

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -5,6 +5,7 @@
 // Assembly location: C:\.NET\ProfilingTools\DumpHomework\MyCalculator.exe
 
 using System;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Markup;
@@ -13,23 +14,38 @@ namespace MyCalculatorv1
 {
     public partial class MainWindow : Window, IComponentConnector
     {
+        private static char[] operations = new[] { '+', '-', '/', '*' };
         public MainWindow() => this.InitializeComponent();
 
-        private void Button_Click_1(object sender, RoutedEventArgs e) => this.tb.Text += ((Button)sender).Content.ToString();
-
-        private void Result_click(object sender, RoutedEventArgs e) => this.result();
-
-        private void result()
+        private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            int num1 = 0;
-            if (this.tb.Text.Contains("+"))
-                num1 = this.tb.Text.IndexOf("+");
-            else if (this.tb.Text.Contains("-"))
-                num1 = this.tb.Text.IndexOf("-");
-            else if (this.tb.Text.Contains("*"))
-                num1 = this.tb.Text.IndexOf("*");
-            else if (this.tb.Text.Contains("/"))
-                num1 = this.tb.Text.IndexOf("/");
+            var button = (Button)sender;
+            if(tb.Text.Contains("="))
+            {
+                tb.Text = "";
+            }
+
+            if(tb.Text.IndexOfAny(operations) > -1 && operations.Any(o => o == button.Content.ToString().First()) || (tb.Text.Length == 0 && operations.Any( o=> o == button.Content.ToString().First())))
+            {
+                return;
+            }
+
+            this.tb.Text += button.Content.ToString();
+        }
+
+        private void Result_click(object sender, RoutedEventArgs e) => this.Result();
+
+        private void Disable_Keyboard(object sender, RoutedEventArgs e) => e.Handled = true;
+
+        private void Result()
+        {
+            if (this.tb.Text.IndexOf("=") > -1 || operations.Any(o=>o == tb.Text.Last()) || this.tb.Text.IndexOfAny(operations) < 0)
+            {
+                return;
+            }
+
+            int num1 = tb.Text.IndexOfAny(operations);
+
             string str = this.tb.Text.Substring(num1, 1);
             double num2 = Convert.ToDouble(this.tb.Text.Substring(0, num1));
             double num3 = Convert.ToDouble(this.tb.Text.Substring(num1 + 1, this.tb.Text.Length - num1 - 1));
